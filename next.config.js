0cc2368d1c2b8aed4777f8ef6005511c58b25468
TheMehranKhan <khanmohammadimehran@gmail.com>
2024-09-16 17:19:25 +0330

    Add Next.js config and update image references

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '/themehrankhan.github.io/',
+    basePath: '/themehrankhan.github.io',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
